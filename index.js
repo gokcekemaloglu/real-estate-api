e13b3e73c9f022da5b1bc9d37736d024712f9d5f
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-04-01 20:58:03 +0300

    feat: Added MongoDB and Mongoose dependencies and configurations to the project. Setup express server, environment configuration and mongodb connection

--- index.js
+++ index.js
@@ -11,8 +11,25 @@ require("dotenv").config()
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
 
+/* ----------------------------------- */
+// Configrations:
+
+// DB Connection:
+const {dbConnection} = require ("./src/configs/dbConnection")
+dbConnection()
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
 app.use(express.json())
 
+// RUN SERVER:
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+
+/* ------------------------------------------------------- */
+
+/*
 app.get("/", (req, res) => {
   res.send("Real Estate API running")
 })
@@ -20,3 +37,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+  */
