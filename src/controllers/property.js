f6d7d71def103944594d6b1d28b3fceeb6d5f9c6
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-04-23 13:45:35 +0300

    feat: Implement list and create methods in Property controller

--- src/controllers/property.js
+++ src/controllers/property.js
@@ -9,8 +9,27 @@ const Property = require("../models/property")
 const {mongoose} = require("../configs/dbConnection")
 
 module.exports = {
-    list: async (req, res) => {},
-    create: async (req, res) => {},
+    list: async (req, res) => {
+        const data = await res.getModelList(Property, {}, "ownerId")
+        const details= await res.getModelListDetails(Property)
+        // console.log(req);
+        // console.log(res);
+        
+        res.status(200).send({
+            error: false,
+            // message: req.,
+            details,
+            data
+        })
+    },
+    create: async (req, res) => {
+        const data = await Property.create(req.body)
+        res.status(201).send({
+            error: false,
+            message: "Property created successfully",
+            data
+        })
+    },
     read: async (req, res) => {},
     update: async (req, res) => {},
     delete: async (req, res) => {},
